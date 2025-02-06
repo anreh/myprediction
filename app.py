4e9918dd8a67c9768253c0904752d4801ddb9516
anreh <39509583+anreh@users.noreply.github.com>
2025-02-06 12:20:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import shap
 import pandas as pd
 import numpy as np
-import openai
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
